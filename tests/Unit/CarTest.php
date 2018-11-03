b40cf214dd175739090b3264f653ff57045741d8
aniket mirajkar <aam257@njit.edu>
2018-11-03 19:13:19 -0400

    Task:  Created a unit test to test inserting a car.

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use App\Cars;
+
+class CarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testInserCartest()
+    {
+        $car = new Cars();
+        $car->make = 'Honda';
+        $car->model = 'Ennova';
+        $car->year = '2010';
+        $this->assertTrue($car->save());
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
